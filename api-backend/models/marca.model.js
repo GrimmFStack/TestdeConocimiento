f5a311a402315a08a3558a15fbc70c2bba78c466
Luis Pech <luisalfonsoekpech@gmail.com>
2025-05-06 10:47:50 -0600

    Delete api-backend directory

--- api-backend/models/marca.model.js
+++
@@ -1,9 +0,0 @@
-const { DataTypes } = require("sequelize");
-
-module.exports = (sequelize) => {
-  return sequelize.define("Marca", {
-    nombre: DataTypes.STRING,
-    activa: { type: DataTypes.BOOLEAN, defaultValue: true },
-    eliminado: { type: DataTypes.BOOLEAN, defaultValue: false }
-  });
-};
